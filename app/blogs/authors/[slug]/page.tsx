22fd3c5a8cefeb483d99736a5678434ddb6d1cba
mdratul4505 <dev.raul.coder@gmail.com>
2026-07-19 01:05:17 +0600

    create a nested route

---
+++ app/blogs/authors/[slug]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const slugPage = () => {
+  return (
+    <div>slugPage</div>
+  )
+}
+
+export default slugPage
---
+++ app/blogs/authors/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const authorPages = () => {
+  return (
+    <div>authorPages</div>
+  )
+}
+
+export default authorPages
+
