493b3fa4e42e4c7c8ca8fe3e017d1d86ce8fa571
mdratul4505 <dev.raul.coder@gmail.com>
2026-07-21 02:33:39 +0600

    added my project shadcn

--- app/blogs/authors/[slug]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const slugPage = () => {
-  return (
-    <div>slugPage</div>
-  )
-}
-
-export default slugPage
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,9 @@
 
 import "./globals.css";
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 
 export default function RootLayout({
@@ -10,10 +14,10 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={` h-full antialiased`}
+      className={cn("h-full", "antialiased", "font-sans", inter.variable)}
     >
       <body className="min-h-full flex flex-col">
-        navbar
+
         {children}
         </body>
     </html>
