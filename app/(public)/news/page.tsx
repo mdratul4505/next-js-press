1f21e721961b6ace105712204f94ca8a75414f4f
mdratul4505 <dev.raul.coder@gmail.com>
2026-07-21 03:22:00 +0600

    added sove componet page

--- app/(public)/news/page.tsx
+++ app/(public)/news/page.tsx
@@ -1,9 +1,10 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const NewsPage = () => {
   return (
     <div>NewsPage
-    
+    <Button>hello word i am md ratul hasan</Button>
     </div>
   )
 }
