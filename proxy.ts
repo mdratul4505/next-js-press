e52f3377c89a00333abae53023ec945d380b2c6c
mdratul4505 <dev.raul.coder@gmail.com>
2026-07-24 02:05:55 +0600

    emplemnt public route wit proxy

--- proxy.ts
+++ proxy.ts
@@ -1,8 +1,10 @@
 import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextRequest } from 'next/server'
+import jwt, { JwtPayload } from "jsonwebtoken"
 
 const AUTH_ROUTE = ["/login", "/register"]
+const PUBLIC_ROUTE = ["/", "/news" ,"/login", "/register"]
 
 // This function can be marked `async` if using `await` inside
 export async function proxy(request: NextRequest) {
@@ -13,6 +15,30 @@ export async function proxy(request: NextRequest) {
 
     const accessToken = request.cookies.get("accessToken")?.value
 
+    const decodedToken = accessToken ? jwt.decode(accessToken) as JwtPayload : null ;
+    let userRole = null;
+
+    if(decodedToken){
+        userRole = decodedToken.role;
+    }
+    if(accessToken && AUTH_ROUTE.includes(pathName)){
+        if(userRole === "USER"){
+            return NextResponse.redirect( new URL('/dashboard' , request.url))
+        }else if (userRole === "ADMIN"){
+           return NextResponse.redirect( new URL('/admin-dashboard' , request.url)) 
+        }else if (userRole === "AUTHOR"){
+            return NextResponse.redirect( new URL('/author-dashboard' , request.url))
+        }else{
+            return NextResponse.redirect( new URL('/' , request.url))
+        }
+    }
+        
+
+    const isPublic = PUBLIC_ROUTE.some((route)=> pathName === route || pathName.startsWith(route + "/")) ;
+
+    if(!accessToken && isPublic){
+        return NextResponse.redirect( new URL('/login' , request.url))
+    }
 
 //   return NextResponse.redirect(new URL('/', request.url))
  return NextResponse.next()
